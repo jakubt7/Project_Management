934aaf0aaf64b223ae68a76968d7ef6a5bf9f3c9
jakubt7 <jakub.tomaszewski.jj@gmail.com>
2023-11-02 22:54:34 +0100

    Updated table view Employees and Projects

--- project-management-app/src/components/EmployeeComponents/EmployeeTable/EmployeeTable.jsx
+++ project-management-app/src/components/EmployeeComponents/EmployeeTable/EmployeeTable.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import "./EmployeeTable.scss";
 import { Link } from "react-router-dom";
+import "tailwindcss/tailwind.css";
 
 const EmployeeTable = () => {
   const [data, setData] = useState([]);
@@ -52,45 +52,61 @@ const EmployeeTable = () => {
 
   
   return (
-    <div>
-      <div className="aboveTable">
-        <h2>Employee List</h2>
-        <Link to={"/employees/create"} className="btn">
-          Add Employee
-        </Link>
+    <div className="flex justify-center h-screen mt-5">
+      <div className="w-5/6">
+        <div className="bg-white shadow-md rounded-lg mb-4 p-4">
+          <div className="flex justify-between items-center">
+            <h2 className="text-2xl font-2xl text-gray-800">Employee List</h2>
+            <Link to="/employees/create" className="btn bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg">
+              Add Employee
+            </Link>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow-md">
+          <table className="min-w-full m-auto">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                  First Name
+                </th>
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                  Last Name
+                </th>
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                  Position
+                </th>
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                  Email
+                </th>
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                  Details
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((employee) => (
+                <tr key={employee.employee_id} className="border-b hover:bg-gray-100">
+                  <td className="px-6 py-4 whitespace-nowrap">{employee.employee_name}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{employee.employee_lastname}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{employee.employee_position}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">{employee.employee_email}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <Link to={`/employees/${employee.employee_id}`} className="text-blue-500 hover:underline hover:text-blue-700">
+                      More
+                    </Link>
+                    <Link to={`/employees/edit/${employee.employee_id}`} className="text-gray-500 hover:underline hover:text-gray-700 ml-4">
+                      Edit
+                    </Link>
+                    <button className="text-red-500 hover:underline hover:text-red-700 ml-4" onClick={() => handleDelete(employee.employee_id)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Position</th>
-            <th>Email</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((employee) => (
-            <tr key={employee.employee_id}>
-              <td>{employee.employee_name}</td>
-              <td>{employee.employee_lastname}</td>
-              <td>{employee.employee_position}</td>
-              <td>{employee.employee_email}</td>
-              <td>
-                <Link to={`/employees/${employee.employee_id}`}>
-                    <button>More</button>
-                </Link>
-                <button>
-                  Edit
-                </button>
-                <button onClick={() => handleDelete(employee.employee_id)}>
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   );
 };
--- project-management-app/src/components/ProjectComponents/ProjectTable/ProjectTable.jsx
+++ project-management-app/src/components/ProjectComponents/ProjectTable/ProjectTable.jsx
@@ -58,45 +58,52 @@ const ProjectTable = () => {
     }
   
     return (
-      <div>
-        <div className="aboveTable">
-          <h2>Project List</h2>
-          <Link to={"/projects/create"} className="btn">
-            Add Project
-          </Link>
-        </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Status</th>
-              <th>Starting date</th>
-              <th>Finishing date</th>
-              <th>Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((project) => (
-              <tr key={project.project_id}>
-                <td>{project.project_name}</td>
-                <td>{project.project_status_name}</td>
-                <td>{formatDate(project.start_date)}</td>
-                <td>{formatDate(project.end_date)}</td>
-                <td>
-                  <Link to={`/projects/${project.project_id}`}>
-                      <button>More</button>
-                  </Link>
-                  <button>
-                  Edit
-                </button>
-                  <button onClick={() => handleDelete(project.project_id)}>
-                    Delete
-                  </button>
-                </td>
+      <div className="flex justify-center mt-5">
+        <div className="w-5/6">
+          <div className="bg-white p-4 shadow-md rounded-lg mb-4">
+            <div className="flex justify-between items-center">
+              <h2 className="text-2xl font-2xl text-gray-800">Project List</h2>
+              <Link to="/projects/create" className="btn bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg">
+                Add Project
+              </Link>
+            </div>
+          </div>
+          <table className="min-w-full m-auto">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">Name</th>
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">Status</th>
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">Starting date</th>
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">Finishing date</th>
+                <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">Details</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {data.map((project) => (
+                <tr key={project.project_id} className="border-b hover:bg-gray-100">
+                  <td className="px-4 py-2 whitespace-nowrap">{project.project_name}</td>
+                  <td className="px-4 py-2 whitespace-nowrap">{project.project_status_name}</td>
+                  <td className="px-4 py-2 whitespace-nowrap">{formatDate(project.start_date)}</td>
+                  <td className="px-4 py-2 whitespace-nowrap">{formatDate(project.end_date)}</td>
+                  <td className="px-4 py-2 whitespace-nowrap">
+                    <Link to={`/projects/${project.project_id}`} className="text-blue-500 hover:underline hover:text-blue-700">
+                      More
+                    </Link>
+                    <Link to={`/projects/edit/${project.project_id}`} className="text-gray-500 hover:underline hover:text-gray-700 ml-4">
+                      Edit
+                    </Link>
+                    <button
+                      className="text-red-500 hover:underline hover:text-red-700 ml-4"
+                      onClick={() => handleDelete(project.project_id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   };
