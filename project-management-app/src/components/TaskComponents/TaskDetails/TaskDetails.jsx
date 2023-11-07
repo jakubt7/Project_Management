668479d16fd060167bfc0b5fd176163b894cdd31
jakubt7 <jakub.tomaszewski.jj@gmail.com>
2023-11-07 01:07:36 +0100

    Task table adjustment and detail view

--- project-management-app/src/components/TaskComponents/TaskDetails/TaskDetails.jsx
+++ project-management-app/src/components/TaskComponents/TaskDetails/TaskDetails.jsx
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from "react";
 import "./TaskDetails.scss";
 import { useParams } from "react-router-dom";
-import Sidebar from "../../Sidebar/Sidebar";
-import Navbar from "../../Navbar/Navbar";
+import AppHeader from "../../AppHeader/AppHeader";
 
 const TaskDetails = () => {
   const { taskId } = useParams();
   const [data, setData] = useState([]);
+  const [assigneeName, setAssigneeName] = useState("");
+  const [employeeName, setEmployeeName] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       try {
         const taskData = await fetch(`http://localhost:8080/tasks/${taskId}`);
 
-        if (!taskData.ok) {
+        if (taskData.ok) {
+          const task = await taskData.json();
+          setData(task);
+          console.log(task);
+
+          fetchEmployeeName(task.assignee_id, setAssigneeName);
+          fetchEmployeeName(task.employee_id, setEmployeeName);
         } else {
-          const data = await taskData.json();
-          setData(data);
-          console.log(data);
         }
       } catch (error) {
         console.error(error.message);
@@ -26,13 +30,26 @@ const TaskDetails = () => {
     fetchData();
   }, [taskId]);
 
+  async function fetchEmployeeName(employeeId, setNameFunction) {
+    try {
+      const employeeData = await fetch(`http://localhost:8080/employees/${employeeId}`);
+
+      if (employeeData.ok) {
+        const employee = await employeeData.json();
+        setNameFunction(`${employee.employee_name} ${employee.employee_lastname}`);
+      } else {
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 
 
   const {
     task_name,
     task_description,
     project_name,
-    team_name,
+    assignee_id,
     employee_id,
     employee_name,
     employee_lastname,
@@ -44,26 +61,41 @@ const TaskDetails = () => {
   function formatDate(inputDate) {
     const date = new Date(inputDate);
     const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
+    return date.toLocaleDateString('en-GB', options);
   }
 
   return (
     <div className="taskDetails">
-      <Sidebar />
-      <div className="detailsContainer">
-        <Navbar />
-        <div className="taskInfo">
-          <div>
-            Task Information
-            <div>{task_name}</div>
-            <div>{task_description}</div>
-            <div>{project_name}</div>
-            <div>{team_name}</div>
-            <div>{employee_name} {employee_lastname}</div>
-            <div>{task_status_name}</div>
-            <div>{formatDate(start_date)}</div>
-            <div>{formatDate(end_date)}</div>
-            <div>{employee_name} {employee_lastname}</div>
+      <AppHeader />
+      <div className="flex justify-center h-screen mt-5">
+        <div className="w-3/5">
+          <div className=" border-solid border-2 border-black p-10 rounded-md">
+          <div className="flex justify-between">
+            <img
+              src="https://via.placeholder.com/200"
+              className="employee-image rounded-2xl w-1/3"
+            />
+            <div className="ml-4 w-2/3 text-center self-center">
+              <h2>
+              {task_name}
+              </h2>
+              <p>{task_description}</p>
+              <p><strong>Project: </strong>{project_name}</p>
+              <p><strong>Assigned to: </strong>{assigneeName}</p>
+              <p><strong>Status: </strong>{task_status_name}</p>
+              <p><strong>Assigned on: </strong>{formatDate(start_date)}</p>
+              <p><strong>Due date: </strong>{formatDate(end_date)}</p>
+              <p><strong>Assigned by: </strong>{employeeName}</p>
+            </div>
+          </div>
+          {/* <div className="employeeTeams mt-5">
+            <h2 className="text-lg font-semibold">Assigned teams</h2>
+            <ul>
+              {data.map((task) => (
+                <li key={task.team_id}>{team_name}</li>
+              ))}
+            </ul>
+          </div> */}
           </div>
         </div>
       </div>
--- project-management-app/src/components/TaskComponents/TaskList/TaskList.jsx
+++ project-management-app/src/components/TaskComponents/TaskList/TaskList.jsx
@@ -26,40 +26,66 @@ const TaskList = () => {
     return (
       <div className="flex justify-center mt-5">
         <div className="w-5/6">
-        <div className="bg-white p-4 shadow-md rounded-lg mb-4">
+          <div className="bg-white p-4 shadow-md rounded-lg mb-4">
             <div className="flex justify-between items-center">
               <h2 className="text-2xl font-2xl text-gray-800">Task List</h2>
-              <Link to="/projects/create" className="btn bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg">
+              <Link to="/projects/create" className="btn bg-blue-500 hover-bg-blue-600 text-white py-2 px-4 rounded-lg">
                 Add Task
               </Link>
             </div>
           </div>
-        <div>
-          <div className="flex justify-between text-center mb-2">
-            <div className='w-1/4'>Task ID</div>
-            <div className='w-1/4'>Project</div>
-            <div className='w-1/4'>Assigned</div>
-            <div className='w-1/4'>Status</div>
-            </div>
-          {data.map((task) => (
-            <Link
-              to={`/tasks/${task.task_id}`}
-              key={task.task_id}
-            >
-              <div className="bg-gray-100 rounded-lg p-3 hover:bg-gray-300 mb-4 border border-slate-600">
-                    <div className="flex justify-between font-medium text-center">
-                      <div className='w-1/4'>{task.task_name}</div>
-                      <div className='w-1/4'>{task.project_name}</div>
-                      <div className='w-1/4'>{task.employee_name} {task.employee_lastname}</div>
-                      <div className='w-1/4'>{task.task_status_name}</div>
-                    </div>
-                  </div>
-            </Link>
-          ))}
+          <div className="bg-white rounded-lg shadow-md">
+            <table className="min-w-full m-auto">
+              <thead>
+                <tr className="bg-gray-100">
+                  <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                    Task ID
+                  </th>
+                  <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                    Project
+                  </th>
+                  <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                    Assigned
+                  </th>
+                  <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                    Status
+                  </th>
+                  <th className="px-6 py-3 text-xs font-medium text-gray-800 uppercase tracking-wider">
+                    Details
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((task) => (
+                    <tr className="border-b hover:bg-gray-100" key={task.task_id}>
+                      <td className="px-4 py-2 whitespace-nowrap">{task.task_name}</td>
+                      <td className="px-4 py-2 whitespace-nowrap">{task.project_name}</td>
+                      <td className="px-4 py-2 whitespace-nowrap">{task.employee_name} {task.employee_lastname}</td>
+                      <td className="px-4 py-2 whitespace-nowrap">{task.task_status_name}</td>
+                      <td className="px-4 py-2 whitespace-nowrap">
+                    <Link to={`/tasks/${task.task_id}`} className="text-blue-500 hover:underline hover:text-blue-700">
+                      More
+                    </Link>
+                    <Link to={''} className="text-gray-500 hover:underline hover:text-gray-700 ml-4">
+                      Edit
+                    </Link>
+                    <button
+                      className="text-red-500 hover:underline hover:text-red-700 ml-4"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                    </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
     );
+    
+    
+    
 }
 
 export default TaskList
